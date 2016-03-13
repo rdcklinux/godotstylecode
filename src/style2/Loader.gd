bc2d99d112963ced33582ab4f3923d2a7e5fab96
Roderick Lagunas <roderick.lagunas@sioschile.net>
2016-03-13 16:46:28 -0300

    improve style2

--- src/style2/Loader.gd
+++ src/style2/Loader.gd
@@ -1,7 +1,7 @@
 
 extends Node
 
-
+#declare here all scripts that you use like parent for extend in your class
 func _init():
 	preload("Container.gd")
 	pass
--- src/style2/Style2.gd
+++ src/style2/Style2.gd
@@ -1,6 +1,6 @@
 
 extends Node
-
+onready var Character = preload("Character.gd")
 onready var player = Character.Player.new(get_node("Player"))
 onready var enemy = Character.Enemy.new(get_node("Enemy"))
 
